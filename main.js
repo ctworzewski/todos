a50ba345186d0bdcb7e72f692cd15412a5dd52bc
Cezary Tworzewski <cezary.tworzewski@gmail.com>
2022-08-10 07:33:50 +0200

    Correct

--- main.js
+++ main.js
@@ -9,9 +9,9 @@ addTask.addEventListener('click', handleClick);
 function handleClick(e) {
     let item = document.createElement('li');
     console.log(e.target);
-    value = input.value
-    ul.appendChild(item);
+    value = input.value;
     arr.push(value);
+    ul.appendChild(item);
     console.log(arr);
     input.value = '';
 }
