6037bce59f781a546e3fc1d8b5172fc673d1c036
Cezary Tworzewski <cezary.tworzewski@gmail.com>
2022-08-16 17:57:44 +0200

    Repair my code. Add tasks to lists

--- main.js
+++ main.js
@@ -1,45 +1,22 @@
-const add = document.querySelector('.addTask');
-const input = document.querySelector('.input');
-let ul = document.querySelector('.listTask');
+const input = document.querySelector(".input");
+const btn = document.querySelector(".add");
+const tasksList = document.querySelector(".tasksList");
 
-add.addEventListener('click',  addTask);
+function addTask(e) {
+  const item = document.createElement("li");
+  let value = input.value;
+  const checkbox = document.createElement("input");
+  checkbox.type = "checkbox";
+  const btnDel = document.createElement("button");
+  btnDel.textContent = "Usuń";
 
-function addTask() {
-    let value = input.value;
-    let item = document.createElement('li');
-    let btnDelTask = document.createElement('button');
-    let btnJobDone = document.createElement('button');
+  tasksList.appendChild(item);
 
-    let circleBox = document.createElement('div');
-    let BoxAll = document.createElement('div');
-    
-    if (input.value !== '') {
-        value = input.value;
-        item.innerHTML = value;
-        ul.appendChild(BoxAll);
-        BoxAll.appendChild(circleBox);
-        BoxAll.appendChild(item);
-        BoxAll.appendChild(btnDelTask);
-        BoxAll.appendChild(btnJobDone);
-        BoxAll.classList.add*('boxAll');
-
-        btnJobDone.innerHTML = 'Zakończ';
-        btnDelTask.innerHTML = 'Usuń';
-        btnJobDone.classList.add('job-done');
-        circleBox.classList.add('circle');
-        // btnDelTask.class
-    input.value = '';
-    btnDelTask.addEventListener('click', function() {
-        removeTask(item,btnDelTask);
-    });
-    } else {
-        input.value = 'podaj zadanie';
-        console.log('podaj zadanie');
-    }
+  item.append(checkbox);
+  item.append(value);
+  item.append(btnDel);
+  e.preventDefault();
+  input.value = "";
 }
 
-
-function removeTask(a,b) {
-    ul.removeChild(a);
-    ul.removeChild(b);
-}
+btn.addEventListener("click", addTask);
